7a394079d21d56575d2fed922002745ad7c5a9c3
Ryan Sobol <contact@ryansobol.com>
2016-01-09 12:03:22 -0800

    Simplify spawn example

--- child_process/spawn.js
+++ child_process/spawn.js
@@ -1,13 +1,23 @@
 'use strict';
 
 const spawn = require('child_process').spawn;
+const child = spawn('node', ['--interactive']);
 
-const ls = spawn('ls', ['-hal', '/usr']);
-
-ls.stdout.on('data', (data) => {
-  console.log('stdout:', data.toString());
+// process.stdin.pipe(child.stdin)
+process.stdin.on('data', function(data) {
+  child.stdin.write(data);
 });
 
-ls.on('close', (code) => {
-  console.log('spawned process exited with code', code);
+// child.stdout.pipe(process.stdout)
+child.stdout.on('data', function(data) {
+  process.stdout.write(data);
+});
+
+// child.stderr.pipe(process.stderr)
+child.stderr.on('data', function(data) {
+  process.stderr.write(data);
+});
+
+child.on('close', function(code) {
+  process.exit();
 });
