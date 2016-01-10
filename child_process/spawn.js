94d666285a63e3838d3a0b26917dd56742314e23
Ryan Sobol <contact@ryansobol.com>
2016-01-09 16:48:06 -0800

    Switch to inherited standard streams for spawn example

--- child_process/spawn.js
+++ child_process/spawn.js
@@ -1,22 +1,22 @@
 'use strict';
 
 const spawn = require('child_process').spawn;
-const child = spawn('node', ['--interactive']);
+const child = spawn('node', ['--interactive'], { stdio: 'inherit' });
 
 // process.stdin.pipe(child.stdin)
-process.stdin.on('data', function(data) {
-  child.stdin.write(data);
-});
+// process.stdin.on('data', function(data) {
+//   child.stdin.write(data);
+// });
 
 // child.stdout.pipe(process.stdout)
-child.stdout.on('data', function(data) {
-  process.stdout.write(data);
-});
+// child.stdout.on('data', function(data) {
+//   process.stdout.write(data);
+// });
 
 // child.stderr.pipe(process.stderr)
-child.stderr.on('data', function(data) {
-  process.stderr.write(data);
-});
+// child.stderr.on('data', function(data) {
+//   process.stderr.write(data);
+// });
 
 child.on('close', function(code) {
   process.exit();
